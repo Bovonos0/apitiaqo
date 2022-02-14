15759473f841abb72502399d63c747e0fe26c9d8
Bovonos0 <71797403+Bovonos0@users.noreply.github.com>
2022-02-14 16:23:09 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+os.system("pip3 install flask -U")
+from flask import *
+
+app = Flask(__name__)
+@app.route("/")
+def home(): return "Hello from heroku"
+
+if __name__ == '__main__' app.run("0.0.0.0",7251)
